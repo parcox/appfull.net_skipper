87e7b83d8e93034038e10b7c9475220ae49c361d
Fitri Wibowo <parcoxs@gmail.com>
2014-01-02 14:48:26 +0700

    Initial commit

---
+++ skip_appfullnet.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name Skip appfull.net redirect link
+// @namespace http://fitri.wibowo.uni.me
+// @description Skip the wait time / ads while going to external link on appfull.net
+// @include http://www.appfull.net/go*
+// ==/UserScript== 
+
+document.body.style.background = "#ffffff";
